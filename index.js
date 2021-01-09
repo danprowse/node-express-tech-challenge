1173fb6c376f429c4296f31309d6cf416f411338
Dan Prowse <danprowse@hotmail.co.uk>
2021-01-09 18:48:51 +0000

    CRUD routes implemented for orders

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const orders = require("./db/test-data");
-
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser'); 
 const morgan = require('morgan');
-const helmet = require('helmet')
+const helmet = require('helmet');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(morgan('combined'));
+
+// api routes 
+app.use('/api/orders', require('./routes/api/orders'))
 
 //endpoint
 app.get('/', (req, res) => {
   res.status(200);
-  res.json(orders)
+  res.json({msg: 'Vehicle Order API'})
 })
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
---
+++ routes/api/orders.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const orders = require('../../db/test-data');
+
+router.get('/', (req , res) => {
+  res.status(200);
+  res.json(orders);
+})
+
+// get single order
+router.get('/:id', (req, res) => {
+  const found = orders.some(order => order.id === parseInt(req.params.id));
+  if (found) {
+    res.status(200).json(orders.filter(order => order.id === parseInt(req.params.id)))
+  } else {
+    res.status(404).json({ msg: `order does not exist with the id of: ${req.params.id}`});
+  }
+})
+
+// create order
+router.post('/', (req, res) => {
+  const newOrder = {
+    id: (orders.length + 1),
+    vehicleManufacturer: '',
+    model: '',
+    totalPrice: 0 
+  }
+
+  orders.push(newOrder);
+  
+  res.status(201).json({
+    msg: 'New order created',
+    newOrderId: newOrder.id
+  });
+})
+
+// update order
+router.put('/:id', (req, res) => {
+  const found = orders.some(order => order.id === parseInt(req.params.id));
+  console.log(req.body);
+  if (found) {
+    const updatedOrder = req.body;
+    orders.forEach(order => {
+      if(order.id === parseInt(req.params.id)) {
+        order.vehicleManufacturer = updatedOrder.vehicleManufacturer ? updatedOrder.vehicleManufacturer : order.vehicleManufacturer;
+        order.model = updatedOrder.model ? updatedOrder.model : order.model;
+        order.totalPrice = updatedOrder.totalPrice ? parseInt(updatedOrder.totalPrice) : order.totalPrice;
+        res.status(200).json({msg: 'Order was updated', order});
+      }
+    }) 
+  } else {
+    res.status(404).json({ msg: `Order does not exist with the id of: ${req.params.id}`});
+  }
+})
+
+// delete member
+router.delete('/:id', (req, res) => {
+  const found = orders.some(order => order.id === parseInt(req.params.id));
+  if (found) {
+    //delete order
+    res.status(200).json({ msg: 'order deleted', orders: orders.filter(order => order.id !== parseInt(req.params.id))});
+  } else {
+    res.status(404).json({ msg: `order does not exist with the id of: ${req.params.id}`});
+  }
+})
+
+module.exports = router;
