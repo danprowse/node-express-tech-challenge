d01cfd4c93034f296453629b45a3330da972e286
Dan Prowse <danprowse@hotmail.co.uk>
2021-01-17 21:53:06 +0000

    connecting sting to db now switching between .env and aws.env

--- src/db/Database.js
+++ src/db/Database.js
@@ -2,13 +2,15 @@ const mongodb = require("mongodb");
 
 class Database {
 
+  dbString = process.env.DBURI || process.env.DATABASE;
+  
   constructor(dbName, collection) {
     this.dbName = dbName;
     this.collection = collection;
   }
 
   async loadConnection() {
-    const client = await mongodb.MongoClient.connect(process.env.DATABASE, {
+    const client = await mongodb.MongoClient.connect(this.dbString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
