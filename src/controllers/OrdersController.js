daf436f57de60e175780c04ba214fc641ad2b79e
Dan Prowse <danprowse@hotmail.co.uk>
2021-01-17 19:21:56 +0000

    error handling of mongoDB calls

--- src/controllers/OrdersController.js
+++ src/controllers/OrdersController.js
@@ -4,32 +4,48 @@ const mongodb = require("mongodb");
 // get all orders
 exports.baseRoute = async (req, res) => {
   const orders = await loadOrdersCollection();
-  const allOrders = await orders.find({}).toArray();
-  res.status(200).json(allOrders);
+  try {
+    const allOrders = await orders.find({}).toArray();
+    res.status(200).json(allOrders);
+  } catch (err) {
+    res.status(404).json({ msg: err });
+  }
 };
 
 // get single order
 exports.getSingle = async (req, res) => {
   const orders = await loadOrdersCollection();
-  const order = await orders.findOne({ orderId: req.params.id });
-  res.status(200).json(order);
+  await orders
+    .findOne({ orderId: req.params.id })
+    .then((order) => {
+      res.status(200).json(order);
+    })
+    .catch((err) => {
+      res.status(404).json({ msg: err });
+    });
 };
 
 //create new order
 exports.createSingle = async (req, res) => {
   const orders = await loadOrdersCollection();
   const newId = nanoid.nanoid();
-  await orders.insertOne({
-    orderId: newId,
-    vehicleManufacturer: "",
-    model: "",
-    totalPrice: 0,
-    orderTimestamp: Math.floor(Date.now() / 1000),
-  });
-  res.status(201).json({
-    msg: "New order created",
-    newOrderId: newId,
-  });
+  await orders
+    .insertOne({
+      orderId: newId,
+      vehicleManufacturer: "",
+      model: "",
+      totalPrice: 0,
+      orderTimestamp: Math.floor(Date.now() / 1000),
+    })
+    .then(() => {
+      res.status(201).json({
+        msg: "New order created",
+        newOrderId: newId,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({ msg: err });
+    });
 };
 
 // delete single order
@@ -56,21 +72,24 @@ exports.updateSingle = async (req, res) => {
   const updatedOrder = req.body;
   const updateDocument = {
     $set: {
-      vehicleManufacturer: req.body.vehicleManufacturer,
-      model: req.body.model,
-      totalPrice: req.body.totalPrice
+      vehicleManufacturer: updatedOrder.vehicleManufacturer,
+      model: updatedOrder.model,
+      totalPrice: updatedOrder.totalPrice,
     },
   };
   // if doc to update is found update doc
   if (exist) {
-    try {
-      await orders.updateOne({ orderId: req.params.id }, updateDocument);
-      res.json({ msg: "order updated"})
-    } catch (err) {
-      res.json({
-        error: err.message,
+    await orders
+      .updateOne({ orderId: req.params.id }, updateDocument)
+      .then(() => {
+        // TODO: send back order to prove its been updated
+        res.json({ msg: "order updated" });
+      })
+      .catch((err) => {
+        res.json({
+          error: err
+        });
       });
-    }
   } else {
     // doc not found
     res.status(404).json({
