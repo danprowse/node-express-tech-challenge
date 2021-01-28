c175d0532959f59f0ab90a8ec50a5e90e7adbe25
Dan Prowse <danprowse@hotmail.co.uk>
2021-01-28 18:00:48 +0000

    Removed jwt.verify on endpoints as its being called via verifyToken as middleware

--- src/routes/api/orders.js
+++ src/routes/api/orders.js
@@ -4,58 +4,18 @@ const jwt = require("jsonwebtoken");
 const verifyToken = require("../../auth/auth");
 const ordersController = require("../../controllers/OrdersController");
 
-router.get("/", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      ordersController.baseRoute(req, res);
-    }
-  });
-});
+router.get("/", verifyToken, (req, res) => ordersController.baseRoute(req, res));
 
 // get single order
-router.get("/:id", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      ordersController.getSingle(req, res);
-    }
-  });
-});
+router.get("/:id", verifyToken, (req, res) => ordersController.getSingle(req, res));
 
 // create order
-router.post("/", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      ordersController.createSingle(req, res);
-    }
-  });
-});
+router.post("/", verifyToken, (req, res) => ordersController.createSingle(req, res));
 
 // update order
-router.put("/:id", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      ordersController.updateSingle(req, res);
-    }
-  });
-});
+router.put("/:id", verifyToken, (req, res) => ordersController.updateSingle(req, res));
 
 // delete member
-router.delete("/:id", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      ordersController.deleteSingle(req, res);
-    }
-  });
-});
+router.delete("/:id", verifyToken, (req, res) => ordersController.deleteSingle(req, res));
 
 module.exports = router;
